6bc47718a5db3e2905b8cbfa8041cae105e959b5
kampersanda <shnsk.knd@gmail.com>
2018-10-22 14:19:47 +0900

    add tiny test

--- test/sparse_set_test.cpp
+++ test/sparse_set_test.cpp
@@ -13,7 +13,7 @@ using namespace poplar::test;
 
 constexpr uint64_t N = 10000;
 
-TEST(sparse_set_test, Tiny) {
+void test_select(uint64_t factor) {
   std::vector<bool> bits;
   std::vector<uint64_t> selects;
   sparse_set set;
@@ -22,7 +22,7 @@ TEST(sparse_set_test, Tiny) {
     std::random_device rnd;
     for (uint64_t i = 0; i < N; ++i) {
       uint64_t x = rnd() % 100;
-      if (x < 50) {
+      if (x < factor) {
         bits.push_back(true);
         set.append(true);
         selects.push_back(i);
@@ -36,6 +36,16 @@ TEST(sparse_set_test, Tiny) {
   for (uint64_t i = 0; i < selects.size(); ++i) {
     ASSERT_EQ(selects[i], set.select(i));
   }
+}
+
+TEST(sparse_set_test, Tiny10) {
+  test_select(10);
+}
+TEST(sparse_set_test, Tiny20) {
+  test_select(20);
+}
+TEST(sparse_set_test, Tiny50) {
+  test_select(50);
 }
 
 }  // namespace
